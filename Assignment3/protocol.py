62d64dd6ba0883b5eb68417727a996eeba7b8a4c
Andrew Chan <chan.2019@alumni.ubc.ca>
2021-10-27 05:08:27 -0700

    Preliminary code for message encryption, decryption, and integrity verification

--- Assignment3/protocol.py
+++ Assignment3/protocol.py
@@ -1,8 +1,13 @@
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Random import get_random_bytes
+
 class Protocol:
     # Initializer (Called from app.py)
     # TODO: MODIFY ARGUMENTS AND LOGIC AS YOU SEEM FIT
     def __init__(self):
         self._key = None
+        self._messages_used = 0 # I think we said we would ask the user to make a new session key after a vertain number of time/messages, maybe nag them about this if that happens
         pass
 
 
@@ -33,16 +38,49 @@ class Protocol:
 
 
     # Encrypting messages
-    # TODO: IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
+    # : IMPLEMENT ENCRYPTION WITH THE SESSION KEY (ALSO INCLUDE ANY NECESSARY INFO IN THE ENCRYPTED MESSAGE FOR INTEGRITY PROTECTION)
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def EncryptAndProtectMessage(self, plain_text):
-        cipher_text = plain_text
+        iv = get_random_bytes(16)
+        cipher = AES.new(self._key, AES.MODE_CBC, iv=iv) # CBC mode needs an iv that is pseudorandom.
+        h = SHA256.new()
+
+        text_bytes = plain_text.encode('UTF-8')
+
+        h.update(text_bytes)
+        hash_bytes = h.digest()
+
+        mssg_bytes = hash_bytes + text_bytes
+        ciph_bytes = cipher.encrypt(mssg_bytes)
+        cipher_text = iv + ciph_bytes
+
+        self._messages_used += 1
+        # if (self._messages_used > message threshold || initial auth message time - current time > time threshold) remind user to make a new session TODO
+
+        print (cipher_text) # Temporary print since there is no decrypt yet.
         return cipher_text
 
 
     # Decrypting and verifying messages
-    # TODO: IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
+    # : IMPLEMENT DECRYPTION AND INTEGRITY CHECK WITH THE SESSION KEY
     # RETURN AN ERROR MESSAGE IF INTEGRITY VERITIFCATION OR AUTHENTICATION FAILS
     def DecryptAndVerifyMessage(self, cipher_text):
-        plain_text = cipher_text
+        iv = cipher_text[0:16]
+        ciph_bytes = cipher_text[16:]
+
+        cipher = AES.new(self._key, AES.MODE_CBC, iv=iv)
+        h = SHA256.new()
+
+        mssg_bytes = AES.decrypt(ciph_bytes)
+        hash_bytes = mssg_bytes[0:64]
+        text_bytes = mssg_bytes[64:]
+
+        h.update(text_bytes)
+        if (hash_bytes != h.digest())
+            return 'Integrity is not confirmed, due to hash mismatch'
+
+        self._messages_used += 1
+        # if (self._messages_used > message threshold || initial auth message time - current time > time threshold) remind user to make a new session TODO
+
+        plain_text = text_bytes.decode('UTF-8')
         return plain_text
